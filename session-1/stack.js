c646a9df16264b5e28d865133f0bf9b26f2dbaaf
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-03 15:20:33 +0800

    prettier

--- session-1/stack.js
+++ session-1/stack.js
@@ -1,30 +1,28 @@
 class Stack {
-    constructor() {
-        this.stack = [];
-    }
+  constructor() {
+    this.stack = [];
+  }
 
-    push(n) {
-        this.stack.push(n);
-    }
+  push(n) {
+    this.stack.push(n);
+  }
 
-    pop(n) {
-        if(this.stack.length === 0)
-            console.log("Stack is currently empty.")
-        else
-            this.stack.pop(n);
-    }
+  pop(n) {
+    if (this.stack.length === 0) console.log("Stack is currently empty.");
+    else this.stack.pop(n);
+  }
 
-    check() {
-        console.log(this.stack);
-    }
+  check() {
+    console.log(this.stack);
+  }
 }
 
-const stack = new Stack()
-stack.push("Milk")
-stack.push("Eggs")
-stack.check()  // [“Milk”, “Eggs”]
-stack.pop()
-stack.check()  // [“Milk”]
-stack.pop()
-stack.check()  // []
-stack.pop()    // “Stack is currently empty.”
+const stack = new Stack();
+stack.push("Milk");
+stack.push("Eggs");
+stack.check(); // [“Milk”, “Eggs”]
+stack.pop();
+stack.check(); // [“Milk”]
+stack.pop();
+stack.check(); // []
+stack.pop(); // “Stack is currently empty.”
