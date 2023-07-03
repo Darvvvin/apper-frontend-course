9f3ad07ce511be02045b8fc822ccc6e4fa4bff92
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-03 16:57:46 +0800

    Updated reverse.js

--- session-1/reverse.js
+++ session-1/reverse.js
@@ -1,9 +1,16 @@
 function reverse(word) {
-  return word
-    .split(" ")
-    .map((w) => w.split("").reverse().join(""))
-    .join(" ")
-    .toString();
+  // return word
+  //   .split(" ")
+  //   .map((w) => w.split("").reverse().join(""))
+  //   .join(" ")
+  //   .toString();
+  let finalWord = ''
+
+  for(let i = 0; i < word.length; i++) {
+    finalWord += word[word.length - 1 - i];
+  }
+
+  return finalWord
 }
 
 const word = "hello world";
