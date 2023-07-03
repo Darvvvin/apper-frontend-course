455786274a1d34dec1381415c804bef9631ffc3e
Jacob Darvin <jacobisdarvin@gmail.com>
2023-07-03 15:10:37 +0800

    added transform.js

---
+++ session-1/transform.js
@@ -0,0 +1,12 @@
+function square(n) {
+    return n * n;
+}
+
+function transform(nums) {
+  squaredArray = nums.map(n => square(n));
+  return squaredArray.sort(function(a, b){return a-b});
+}
+
+const nums = [4,9,5,3,8]
+const sortedSquaredNums = transform(nums)
+console.log(sortedSquaredNums) // [9,16,25,64,81]
